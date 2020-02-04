2e62df987f21801a8b51be55e64e07057e51cd2f
Vijai <vijaiyer2000@gmail.com>
2020-02-04 19:33:18 +0530

    FCM Integration

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { AsyncStorage , Alert} from 'react-native';
+import firebase from 'react-native-firebase';
 import AccountScreen from './src/Screens/AccountScreen';
 import React ,{Component} from 'react'
 import OrderScreen from './src/Screens/OrderScreen';
@@ -14,6 +16,7 @@ import Home from './src/Screens/home';
 import Login from './src/Screens/Login';
 import Product from './src/Screens/Product';
 import Schedule from './src/Screens/Schedule';
+import PaytmGateway from './src/Screens/PaytmGateway';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 const AppNavigator = createStackNavigator({
@@ -32,6 +35,7 @@ const AppNavigator = createStackNavigator({
   Login:Login,
   Product:Product,
   Schedule:Schedule,
+  Paytm:PaytmGateway,
   },
 {
   initialRouteName: 'Account',
@@ -41,6 +45,148 @@ const AppNavigator = createStackNavigator({
 });
 const AppContainer = createAppContainer(AppNavigator);
 export default class App extends Component {
+  
+  
+  
+  async componentDidMount() {
+    /* Checking for permissions and creating Notification Listeners */
+    this.checkPermission();
+    this.createNotificationListeners();
+    this.createNotificationChannels();
+  }
+  //Remove listeners allocated in createNotificationListeners()
+
+componentWillUnmount() {
+  /* Deleting Listeners when component is Unmounted*/
+  this.notificationListener();
+  this.notificationOpenedListener();
+
+}
+async createNotificationChannels()
+{
+  // Build a channel
+ const channel = new firebase.notifications.Android.Channel('main', 'MAIN-CHANNEL', firebase.notifications.Android.Importance.Max)
+       .setDescription('My apps test channel');
+// Create the channel
+firebase.notifications().android.createChannel(channel);
+}
+
+async createNotificationListeners() {
+  /*
+
+  * Triggered when a particular notification has been received in foreground
+
+  * */
+  this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      this.showAlert(title, body);
+
+  });
+
+  /*
+
+  * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+
+  * */
+
+  this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+
+      const { title, body } = notificationOpen.notification;
+
+      this.showAlert(title, body);
+
+  });
+
+
+
+  /*
+
+  * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+
+  * */
+
+  const notificationOpen = await firebase.notifications().getInitialNotification();
+
+  if (notificationOpen) {
+
+      const { title, body } = notificationOpen.notification;
+
+      this.showAlert(title, body);
+
+  }
+
+  /*
+
+  * Triggered for data only payload in foreground
+
+  * */
+
+  this.messageListener = firebase.messaging().onMessage((message) => {
+
+    //process data message
+
+    console.log(JSON.stringify(message));
+
+  });
+
+}
+
+
+
+showAlert(title, body) {
+  Alert.alert(
+    title, body,
+    [
+
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+
+    ],
+    { cancelable: false },
+
+  );
+
+}
+  
+  
+  
+/** Function to check permission for FCM**/
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+        this.getToken();
+    } else {
+        this.requestPermission();
+    }
+  }
+  
+  
+  
+/** Function generates and stores fcmToken in AsyncStorage of the Device**/
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    console.log(`fcmToken : ${fcmToken}`);
+    if (!fcmToken) {
+        fcmToken = await firebase.messaging().getToken();
+        if (fcmToken) {
+            // User has a device token
+            await AsyncStorage.setItem('fcmToken', fcmToken);
+        }
+    }
+  }
+  
+  
+  
+    /**Function Request Users for FCM messaging**/
+  async requestPermission() {
+    try {
+        await firebase.messaging().requestPermission();
+        // User has Authorized.
+        this.getToken();
+    } catch (error) {
+        // User has rejected permissions
+        console.log('permission rejected');
+    }}
+
   render() {
     return <AppContainer />;
   }
--- src/Screens/PaymentScreen.js
+++ src/Screens/PaymentScreen.js
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
-import { Container, Header, Title, Button, Icon, Left, Body, Content } from "native-base";
-import{View,StyleSheet,Text ,Image} from 'react-native';
+import { Container, Header, Title, Button, Icon, Left, Body, Content ,CheckBox} from "native-base";
+import{View,StyleSheet,Text ,Image,TouchableNativeFeedback, Alert} from 'react-native';
+import { WebView } from 'react-native-webview'
 export default class PaymentScreen extends Component{
     constructor(props){
         super(props);
+        this.state={
+          cash:false,
+          paytm:false,
+          pressed:false,
+        }
+    }
+    onPressCash(){
+      this.setState({
+        cash:true,
+        paytm:false,
+        pressed:true,
+      });
+    }
+    processPayment(){
+      if(this.state.paytm)
+      {
+        this.props.navigation.navigate('Paytm',{orderID:1,customerId:2,taxationAmount:400})
+      }
+      else
+      {
+        Alert.alert('Order','Order placed',[{text: 'OK', onPress: () => console.log('OK Pressed')}],{cancelable: false});
+      }
+    }
+    onPressPaytm(){
+      this.setState({
+        paytm:true,
+        cash:false,
+        pressed:true,
+      });
     }
   render() {
     return (
@@ -21,22 +51,45 @@ export default class PaymentScreen extends Component{
         </Header>
         <Content>
         <View>
-        <View style={styles.row}>
+        <TouchableNativeFeedback onPress={()=>{this.onPressCash()}}>
+        <View style={styles.row} >
             <View style={styles.item}>
-            <Image source={{uri:"https://www.searchpng.com/wp-content/uploads/2019/02/Google-Pay-Logo-Icon-PNG.png"}} style={styles.image}/>
-            <Text style={styles.name}>GPay</Text>
+            <Image source={{uri:"https://cdn4.iconfinder.com/data/icons/currency-icon-flat-vol-1/100/Indian_Rupee_Currency-512.png"}} style={styles.image}/>
+            <Text style={styles.name}>Cash</Text>
             </View>
-            <Text style={styles.button}>Link Account</Text>
+            <View style={{paddingRight:'5%'}}>
+            <CheckBox checked={this.state.cash}/>
+            </View>
         </View>
+        </TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={()=>{this.onPressPaytm()}}>
         <View style={styles.row}>
             <View style={styles.item}>
             <Image source={{uri:"https://cdn2.iconfinder.com/data/icons/social-icons-color/512/paytm-512.png"}} style={styles.image}/>
             <Text style={styles.name}>Paytm</Text>
             </View>
-            <Text style={styles.button}>Link Account</Text>
+            <View style={{paddingRight:'5%'}}>
+            <CheckBox checked={this.state.paytm} onPress={()=>{this.onPressPaytm()}} />
+            </View>
         </View>
+        </TouchableNativeFeedback>
         </View>
         </Content>
+        <TouchableNativeFeedback onPress={()=>{this.processPayment()}}>
+        <View style={[styles.bottom,this.state.pressed?{opacity:1}:{opacity:0}]}>
+          <Text style={styles.bottom_text}>
+            {this.state.cash?"Place Order":"Pay Now"}
+          </Text>
+        </View>
+        </TouchableNativeFeedback>
+        {
+          this.state.displayWebView?
+          <WebView source={{uri: `http://172.21.0.1:5000/createCheckSum?OrderID=${1234}&CustomerID=${1}&TaxationAmount=${400}`}}
+                  onNavigationStateChange={(navigationState) => {
+                              if (navigationState.title == 'true') {
+                                this.props.navigation.navigate('PaymentResponse', { paymentStatus: navigationState.title })}}}/>
+          :Alert.alert('Order','Order placed',[{text: 'OK', onPress: () => console.log('OK Pressed')}],{cancelable: false},)
+        }
       </Container>
     );
   }
@@ -69,5 +122,23 @@ const styles=StyleSheet.create({
         color:'orange',
         fontSize: 15,
     },
+    bottom:{
+      position:"absolute",
+      bottom:0,
+      left:0,
+      right:0,
+      height:70,
+      paddingHorizontal:20,
+      paddingVertical:10,
+      backgroundColor:'green',
+      marginBottom:-10,
+    },
+    bottom_text:{
+      color:'white',
+      fontSize:22,
+      fontWeight:'100',
+      textAlign:'center',
+      textAlignVertical:"center",
+    }
 
 })
